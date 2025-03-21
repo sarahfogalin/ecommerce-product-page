2f304256337c6376d5b906675f76f771b807af95
Sarah Fogalin <sarahfogalin@gmail.com>
2025-03-21 19:24:52 -0400

    initial commit

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./css/main.css";
+
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default App;
---
+++ components/NavBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavBar = ({}) => {
+  return <div class="nav_bar">Nav bar</div>;
+};
+
+export default NavBar;
